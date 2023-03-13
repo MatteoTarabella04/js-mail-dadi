684a22f6f1f492ca484124e8b01e68fb1d393c2d
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-13 15:32:08 +0100

    add user input + check allow to log

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,24 @@ Mail Bonus:
 Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
 
 */
+
+// Chiedo all'utente la sua mail
+const userMailEl = prompt('inserisci la tua mail');
+console.log(userMailEl);
+
+// creo la lista delle mail con i permessi
+const allowMails = [
+   'pincopallino@bool.it',
+   'pincopallino1@bool.it',
+   'pincopallino2@bool.it',
+   'pincopallino3@bool.it',
+   'pincopallino4@bool.it'
+];
+console.log(allowMails);
+
+// verifico se la mail inserita corrisponde alla lista
+if (allowMails.includes(userMailEl)) {
+   alert('accesso effettuato con successo');
+} else {
+   alert('spiacenti utente non abilitato all accesso');
+}
