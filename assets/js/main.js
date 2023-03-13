880c5546bd39057c4e2b23e3fc4a4d07baa84f9a
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-13 15:54:02 +0100

    add DOM elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@
 
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
-Mail Bonus:
+Mail BONUS:
 Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
 
 */
 
 // Chiedo all'utente la sua mail
-const userMailEl = prompt('inserisci la tua mail');
-console.log(userMailEl);
+//const userMailEl = prompt('inserisci la tua mail');
+//console.log(userMailEl);
 
 // creo la lista delle mail con i permessi
 const allowMails = [
@@ -21,9 +21,29 @@ const allowMails = [
 ];
 console.log(allowMails);
 
-// verifico se la mail inserita corrisponde alla lista
-if (allowMails.includes(userMailEl)) {
-   alert('accesso effettuato con successo');
-} else {
-   alert('spiacenti utente non abilitato all accesso');
-}
+// BONUS seleziono l'input dal DOM
+const userInputMailEl = document.getElementById('userMail');
+console.log(userInputMailEl.value);
+
+// BONUS seleziono l'elemento che stamperà il messaggio dal DOM
+const finalMessageEl = document.getElementById('message');
+
+// BONUS seleziono il bottone a cui aggiungo l'evento di verifica
+const btnEl = document.getElementById('subBtn');
+
+btnEl.addEventListener('click', function () {
+   // verifico se la mail inserita corrisponde alla lista
+   if (allowMails.includes(userInputMailEl.value)) {
+
+      //alert('accesso effettuato con successo');
+      finalMessageEl.innerHTML = `<p> Utente ${userInputMailEl.value} autorizzato all'accesso </p>`;
+
+   } else {
+
+      //alert('spiacenti utente non abilitato all accesso');
+      finalMessageEl.innerHTML = `<p> Utente ${userInputMailEl.value} non autorizzato all'accesso </p>`;
+   }
+
+})
+
+
