ea9ea7f47632be2f10bcc750437776b38a9a577a
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-13 17:39:39 +0100

    add solution js for check mail with loop for

--- MailEx/assets/js/main.js
+++ MailEx/assets/js/main.js
@@ -32,7 +32,9 @@ const finalMessageEl = document.getElementById('message');
 const btnEl = document.getElementById('subBtn');
 
 btnEl.addEventListener('click', function () {
-   
+
+   /* 
+   ---------------- SOLUZIONE CON INCLUDE() ------------------------------------
    // verifico se la mail inserita corrisponde alla lista
    if (allowMails.includes(userInputMailEl.value)) {
 
@@ -42,6 +44,34 @@ btnEl.addEventListener('click', function () {
    } else {
 
       //alert('spiacenti utente non abilitato all accesso');
+      finalMessageEl.innerHTML = `<p> Utente ${userInputMailEl.value} non autorizzato all'accesso </p>`;
+   } 
+   */
+
+   /* -------------------- SOLUZIONE CON CICLO FOR ------------------------- */
+
+   // ASSEGNO AD UNA VARIABILE UN VALORE FALSE (MAIL NON PRESENTE) PER FACILITARE IL CONTROLLO
+   let check = false;
+
+   // SCORRO LA LISTA DELLE MAIL E LE ASSEGNO AD UNA VARIABILE
+   for (let i = 0; i < allowMails.length; i++) {
+      const allowList = allowMails[i];
+
+      if (allowList == userInputMailEl.value) {
+         check = true;
+
+         console.log(check);
+      }
+   }
+
+
+   // VERIFICO SE L'ELEMENTO DELLA LISTA COMBACIA CON L'INPUT UTENTE
+   if (check == true) {
+
+      finalMessageEl.innerHTML = `<p> Utente ${userInputMailEl.value} autorizzato all'accesso </p>`;
+
+   } else {
+
       finalMessageEl.innerHTML = `<p> Utente ${userInputMailEl.value} non autorizzato all'accesso </p>`;
    }
 
